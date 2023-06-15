b3614a14a88fab6ff890adb71ef2de550c0297dd
ameer-ahmad <ahmaamee@sheridancollege.ca>
2023-06-15 11:07:47 -0400

    Fixed search results positioning

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -4,7 +4,7 @@ import './SearchResultsStyles.css'
 const SearchResults = ({ searchPlayers }) => {
   return (
     <div className="search-results">
-        {searchPlayers.slice(0, 15).map((player, pos) => {
+        {searchPlayers.map((player, pos) => {
             return (
                 <div className="player-result" key={pos}>{player.first_name} {player.last_name}</div> 
             )
