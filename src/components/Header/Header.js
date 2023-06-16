605f070ab6d3873b7f7c71871701e8fe9e1decf5
ameer-ahmad <ahmaamee@sheridancollege.ca>
2023-06-16 11:21:12 -0400

    fixed logo issue

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -37,7 +37,7 @@ const Header = () => {
   return (
     <header>
         <div className="header-container">
-            <div className="logo"><img alt="logo" src="./planet-hoops/logo.png"/></div>
+            <div className="logo"><img alt="logo" src="https://ameer-ahmad.github.io/planet-hoops/logo.png"/></div>
             
             <div className="search">
                 <div className="search-container">
